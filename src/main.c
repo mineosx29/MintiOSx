565e2ecff438cbc9b2bca307481d2f3710109d9b
mineosx29 <70161998+mineosx29@users.noreply.github.com>
2020-08-28 17:47:26 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,17 +1,17 @@
-// main.c -- Defines the C-code kernel entry point, calls initialisation routines.
-//           Made for JamesM's tutorials <www.jamesmolloy.co.uk>
+// Basée sur le tutoriel de 
+//  JamesM's tutorials <www.jamesmolloy.co.uk>
 
 #include "monitor.h"
 #include "descriptor_tables.h"
 
 int main(struct multiboot *mboot_ptr)
 {
-    // Initialise all the ISRs and segmentation
+    
     init_descriptor_tables();
     // Initialise the screen (by clearing it)
     monitor_clear();
-    // Write out a sample string
-    monitor_write("Hello, world!\n");
+   
+    monitor_write("Minti: Hello!\n");
 
     asm volatile("int $0x3");
     asm volatile("int $0x4");
