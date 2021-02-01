d2f0a9d63f3d4bff90a2d6185d7f4450ad0e4bd9
mineosx29 <nadir.ziani29@gmail.com>
2021-02-02 00:07:43 +0100

    Fix: some bug of boot and gdt

--- src/boot.s
+++ src/boot.s
@@ -14,32 +14,31 @@ MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
 [BITS 32]                       ; All instructions should be 32-bit.
 
 [GLOBAL mboot]                  ; Make 'mboot' accessible from C.
-[EXTERN code]                   ; Start of the '.text' section.
-[EXTERN bss]                    ; Start of the .bss section.
-[EXTERN end]                    ; End of the last loadable section.
+
 
 mboot:
+align 4
     dd  MBOOT_HEADER_MAGIC      ; GRUB will search for this value on each
                                 ; 4-byte boundary in your kernel file
     dd  MBOOT_HEADER_FLAGS      ; How GRUB should load your file / settings
     dd  MBOOT_CHECKSUM          ; To ensure that the above values are correct
     
-    dd  mboot                   ; Location of this descriptor
-    dd  code                    ; Start of kernel '.text' (code) section.
-    dd  bss                     ; End of kernel '.data' section.
-    dd  end                     ; End of kernel.
-    dd  start                   ; Kernel entry point (initial EIP).
+section .bss
+align 16
+stack_bottom:
+resb 16384
+stack_top:
 
-[GLOBAL start]                  ; Kernel entry point.
-[EXTERN main]                   ; This is the entry point of our C code
+section .text
 
-start:
+[GLOBAL _start]
+[EXTERN main]
+_start:
+    mov esp, stack_top
+    xor ebp, ebp
     ; Load multiboot information:
     push    ebx
 
     ; Execute the kernel:
     cli                         ; Disable interrupts.
-    call main                   ; call our main() function.
-    jmp $                       ; Enter an infinite loop, to stop the processor
-                                ; executing whatever rubbish is in the memory
-                                ; after our kernel!
+   
